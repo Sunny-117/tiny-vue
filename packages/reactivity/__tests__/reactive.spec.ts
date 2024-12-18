85e6920d5c5393033b323acb749719f097aba807
Sunny-117 <zhiqiangfu6@gmail.com>
2024-12-18 14:29:19 +0800

    test: reactivity

--- packages/reactivity/__tests__/reactive.spec.ts
+++ packages/reactivity/__tests__/reactive.spec.ts
@@ -1,13 +1,17 @@
 import { isReactive, reactive, isProxy } from "../src/reactive";
 describe("reactive", () => {
   it("happy path", () => {
-    const original = { foo: 1 };
-    const observed = reactive(original);
-    expect(observed).not.toBe(original);
-    expect(observed.foo).toBe(1);
-    expect(isReactive(observed)).toBe(true);
-    expect(isReactive(original)).toBe(false);
-    expect(isProxy(observed)).toBe(true);
+    const original = { foo: 1 }
+    const observed = reactive(original)
+    expect(observed).not.toBe(original)
+    expect(isReactive(observed)).toBe(true)
+    expect(isReactive(original)).toBe(false)
+    // get
+    expect(observed.foo).toBe(1)
+    // has
+    expect('foo' in observed).toBe(true)
+    // ownKeys
+    expect(Object.keys(observed)).toEqual(['foo'])
   });
 
   test("nested reactives", () => {
