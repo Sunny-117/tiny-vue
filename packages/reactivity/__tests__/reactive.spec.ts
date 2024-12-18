26568cd5d02ad2fea57ed554f61a975a08f08300
Sunny-117 <zhiqiangfu6@gmail.com>
2024-12-18 16:17:19 +0800

    test(reactivity): original value change should reflect in observed value

--- packages/reactivity/__tests__/reactive.spec.ts
+++ packages/reactivity/__tests__/reactive.spec.ts
@@ -49,7 +49,7 @@ describe("reactive", () => {
     expect(cmap).toBeInstanceOf(Map)
     expect(isReactive(cmap)).toBe(true)
   })
-  test.only('observed value should proxy mutations to original (Object)', () => {
+  test('observed value should proxy mutations to original (Object)', () => {
     const original: any = { foo: 1 }
     const observed = reactive(original)
     // set
@@ -58,6 +58,18 @@ describe("reactive", () => {
     expect(original.bar).toBe(1)
     // delete
     delete observed.foo
+    expect('foo' in observed).toBe(false)
+    expect('foo' in original).toBe(false)
+  })
+  test('original value change should reflect in observed value (Object)', () => {
+    const original: any = { foo: 1 }
+    const observed = reactive(original)
+    // set
+    original.bar = 1
+    expect(original.bar).toBe(1)
+    expect(observed.bar).toBe(1)
+    // delete
+    delete original.foo
     expect('foo' in observed).toBe(false)
     expect('foo' in original).toBe(false)
   })
