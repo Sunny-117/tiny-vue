3d7adab6e94bd3ef6f5eb64aef6864d559ba82f5
Sunny-117 <zhiqiangfu6@gmail.com>
2025-05-03 16:50:23 +0800

    chore: recovery code

--- packages/compiler-core/__tests__/transform.spec.ts
+++ packages/compiler-core/__tests__/transform.spec.ts
@@ -3,9 +3,8 @@ import { baseParse } from "../src/parse";
 import { transform } from "../src/transform";
 
 describe("transform", () => {
-  it.only("happy path", () => {
+  it("happy path", () => {
     const ast = baseParse("<div>hi,{{message}}</div>");
-    console.log(ast)
     const plugin = (node) => {
       if (node.type === NodeTypes.TEXT) {
         node.content = node.content + " mini-vue";
